d2bd6695dc3e4724cef60af0923112b6ec313c03
liufeng <w710989327@foxmail.com>
2016-07-16 14:21:40 +0800

    ...

--- mvp/src/main/java/com/iyiyo/mvp/ui/fragment/BaseFragment.java
+++ mvp/src/main/java/com/iyiyo/mvp/ui/fragment/BaseFragment.java
@@ -1,20 +1,33 @@
 package com.iyiyo.mvp.ui.fragment;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.iyiyo.mvp.ui.interf.BaseFragmentInterface;
 import com.iyiyo.nucleus.presenter.Presenter;
 import com.iyiyo.nucleus.view.NucleusFragment;
+
+import java.util.Calendar;
+
+import butterknife.ButterKnife;
 
 
 /**
  * Created by thanatos on 15/12/21.
  */
-public abstract class BaseFragment<P extends Presenter> extends NucleusFragment<P> {
+public abstract class BaseFragment<P extends Presenter> extends NucleusFragment<P>
+        implements View.OnClickListener, BaseFragmentInterface {
 
-    public Context mContext;
+    public Activity mContext;
     public Resources resources;
+    private long lastClickTime = 0;
 
     public static final String BUNDLE_TYPE = "BUNDLE_TYPE";
 
@@ -25,4 +38,32 @@ public abstract class BaseFragment<P extends Presenter> extends NucleusFragment<
         resources = mContext.getResources();
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(getResourceId(), container, false);
+        ButterKnife.bind(this, view);
+        initView(view);
+        initData();
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        if (currentTime - lastClickTime > 1000) {
+            lastClickTime = currentTime;
+            onClick(v);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    public void showToast(String text) {
+        new Handler().obtainMessage(0x1001,text).sendToTarget();
+    }
 }
---
+++ mvp/src/main/java/com/iyiyo/mvp/ui/interf/BaseFragmentInterface.java
@@ -0,0 +1,21 @@
+package com.iyiyo.mvp.ui.interf;
+
+import android.view.View;
+
+
+/**
+ * 基类fragment实现接口
+ * @author FireAnt（http://my.oschina.net/LittleDY）
+ * @created 2014年9月25日 上午11:00:25
+ *
+ */
+public interface BaseFragmentInterface {
+
+	public int getResourceId();
+	
+	public void initView(View view);
+	
+	public void initData();
+
+	public void onClick(View v);
+}
